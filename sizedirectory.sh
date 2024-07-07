761abeda3797e389959e2f94bbf25675865d952e
evgeniy <evg.pg4v@ya.ru>
2024-07-07 15:27:46 +0000

    add file

---
+++ sizedirectory.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# current_dir=$(pwd)
+# for item in "$current_dir"/*
+# do
+	# if [ -d "$item" ]; then
+		# echo "Directory:$item"
+	# elif [ -f "$item" ]; then
+		# echo "File:$item"
+	# fi
+# done
+
+#sudo du -h -Bm --max-depth=1 | sort -nr | head -n11
+#echo "Вывести оставшееся содержимое?Y/N"
+#read bool
+#if [ $bool = Y ]; then
+#	sudo du -h -Bm --max-depth=1 | sort -nr | more +12 -22
+#fi
+
+numb=1
+echo "$numb"
